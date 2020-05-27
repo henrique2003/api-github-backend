4ea2820a3542a4d4ef37870874d04e87c0520f4a
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-26 22:59:53 -0300

    feat: add UserController show

--- src/app/controllers/user-controller.ts
+++ src/app/controllers/user-controller.ts
@@ -8,7 +8,23 @@ class UserController {
 
       return res.status(200).json(users)
     } catch (error) {
-      return res.status(500).json('Server Error')
+      return res.status(500).json(new Error('Server Error'))
+    }
+  }
+
+  public async show (req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params
+
+      if (!id) {
+        return res.status(400).json(new Error('Id não encontrado'))
+      }
+
+      const user = await User.findById(id)
+
+      return res.status(200).json(user)
+    } catch (error) {
+      return res.status(500).json(new Error('Server Error'))
     }
   }
 }
